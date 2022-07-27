d1ecb4de98cf4d23e9f1f3f804f0a1010179a516
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2022-07-27 18:35:24 -0400

    feat: add content

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <name-header></name-header>
+  <bio></bio>
+</template>
+
+<script>
+
+import NameHeader from '../components/NameHeader.vue'
+import Bio from '../components/Bio.vue'
+
+export default {
+    components: {
+        NameHeader,
+        Bio,
+    }
+}
+
+</script>
