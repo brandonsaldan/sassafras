a30492b079a3364936b8b448c638d1501f354550
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2022-07-27 21:12:04 -0400

    feat: add grid

---
+++ src/components/Grid.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="mt-6 grid grid-cols-2 gap-4">
+        <div class="md:flex md:items-center md:justify-between">
+            <div class="flex-1 min-w-0">
+                <h1 class="text-2xl font-medium leading-7 text-gray-100 sm:text-base sm:truncate tracking-tight">Building</h1>
+                <div class="mt-4">
+                    <p class="text-2xl font-base leading-7 text-gray-100 sm:text-sm sm:truncate tracking-tight">Invii</p>
+                    <p class="text-2xl font-base leading-7 text-gray-100 sm:text-sm tracking-tight">An all-in-one restaurant management solution.</p>
+                </div>
+                <div class="mt-6">
+                    <p class="text-2xl font-base leading-7 text-gray-100 sm:text-sm sm:truncate tracking-tight">Dune</p>
+                    <p class="text-2xl font-base leading-7 text-gray-100 sm:text-sm tracking-tight">Raycast-inspired TailwindCSS components.</p>
+                </div>
+            </div>
+        </div>
+        <div class="md:flex md:items-center md:justify-between">
+            <div class="flex-1 min-w-0">
+                <h1 class="text-2xl font-medium leading-7 text-gray-100 sm:text-base sm:truncate tracking-tight">Projects</h1>
+                <div class="mt-4">
+                    <p class="text-2xl font-base leading-7 text-gray-100 sm:text-sm sm:truncate tracking-tight">Sassafras</p>
+                    <p class="text-2xl font-base leading-7 text-gray-100 sm:text-sm tracking-tight">A minimalist portfolio template built with Vue.</p>
+                </div>
+                <div class="mt-6">
+                    <p class="text-2xl font-base leading-7 text-gray-100 sm:text-sm sm:truncate tracking-tight">Fox</p>
+                    <p class="text-2xl font-base leading-7 text-gray-100 sm:text-sm tracking-tight">A username scraping Discord bot.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <name-header></name-header>
   <bio></bio>
+  <grid></grid>
+  <divider></divider>
 </template>
 
 <script>
 
 import NameHeader from '../components/NameHeader.vue'
 import Bio from '../components/Bio.vue'
+import Grid from '../components/Grid.vue'
+import Divider from '../components/Divider.vue'
 
 export default {
     components: {
         NameHeader,
         Bio,
+        Grid,
+        Divider,
     }
 }
 
